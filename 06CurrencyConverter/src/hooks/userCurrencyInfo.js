bc2da64348638e2accc18853511cec77e57a349a
krushna <krushnasakhare965@gmail.com>
2024-03-11 23:14:45 +0530

    Done currency project

---
+++ 06CurrencyConverter/src/hooks/userCurrencyInfo.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+
+// Designed the custom hook
+function useCurrencyInfo(currency){
+//To store the data of currency/API, if used variable then will not displayed the currency on the screen
+   const [data, setData] = useState({})
+
+   useEffect(() =>{
+      fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-02/v1/currencies/${currency}.json`) 
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]))
+   } , [currency])
+
+   console.log(data);
+   return data;
+}
+
+export default  useCurrencyInfo;
